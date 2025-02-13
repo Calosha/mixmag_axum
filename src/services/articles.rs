ad6e15b86fca2b1c2b2ad941370f2244feb3f479
Alexey Chernyuk <alexey.chernyuk@gmail.com>
2025-02-13 00:35:45 -0500

    fix: articles accept pool now

--- src/services/articles.rs
+++ src/services/articles.rs
@@ -1,8 +1,16 @@
 use crate::schema::article::dsl::*;
 use crate::{config::database::DbPool, models::article::Article};
+use axum::response::Json;
+use diesel::prelude::*;
+use serde_json::json;
+
+use crate::schema::article::dsl::*;
 use diesel::prelude::*;
 
-pub async fn get_articles(pool: &DbPool) -> Result<Vec<Article>, diesel::result::Error> {
+pub async fn get_articles(
+    Extension(pool): Extension<DbPool>,
+) -> Result<Json, diesel::result::Error> {
     let mut conn = pool.get().expect("Failed to get DB connection");
-    article.limit(10).load::<Article>(&mut conn)
+    let articles = article.limit(10).load::<Article>(&mut conn)?;
+    Ok(Json(json!(articles)))
 }
